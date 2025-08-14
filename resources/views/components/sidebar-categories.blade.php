f7eb2f440c5eaa982979e0d6788ac2486cc9cbe9
Juanmi <juanmi0802@gmail.com>
2025-08-14 10:28:06 +0200

    temas estilos

--- resources/views/components/sidebar-categories.blade.php
+++ resources/views/components/sidebar-categories.blade.php
@@ -3,7 +3,12 @@
         <h3>Temes</h3>
         <ul id="temas">
             @foreach (category() as $category)
-                <li><a href="{{ route('category', $category->slug) }}">{{$category->name}} </a><small class="posts">{{$category->posts->count()}}</small></li>
+                @php
+                    $text = wordwrap($category->name, 35, "<br>", true);
+                @endphp
+                <li><a href="{{ route('category', $category->slug) }}">{!! $text !!} </a>
+                    <small class="posts">{{$category->posts->count()}}</small>
+                </li>
             @endforeach
         </ul>
     </article>
