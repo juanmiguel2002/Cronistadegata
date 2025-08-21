eb1afcc66e3376020a357e3ae840238e55748b9b
Juanmi <juanmi0802@gmail.com>
2025-08-21 11:18:38 +0200

    rename pageTitle

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,14 +13,14 @@ class AuthController extends Controller
 {
     public function loginForm(Request $request){
         $data = [
-            'pageTitle' => 'Login'
+            'pageTitle' => 'Entrar'
         ];
         return view('back.pages.auth.login', $data);
     }
 
     public function forgotForm(Request $request){
         $data = [
-            'pageTitle' => 'Forgot Password'
+            'pageTitle' => 'Olvidaste tu contraseña',
         ];
         return view('back.pages.auth.forgot', $data);
     }
