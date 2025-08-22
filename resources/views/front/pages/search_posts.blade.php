fbb1eeaf4b8dce5b907f76f7f18594b29c7e1e76
Juanmi <juanmi0802@gmail.com>
2025-08-22 09:39:58 +0200

    corrección estilos busquetda

--- resources/views/front/pages/search_posts.blade.php
+++ resources/views/front/pages/search_posts.blade.php
@@ -9,16 +9,27 @@
     </div>
     @forelse ($posts as $post)
         <article class="card">
-            <h2 class="card-title">{{ date_format($post->created_at, 'd/m/Y') }} <a class="link" href="{{ route('post', $post->slug) }}">{{ $post->title }}</a></h2>
-            <h5 class="temas">en <strong>{{ $post->post_category->name }} </strong>
+            <h2 class="post-title">{{ date_format($post->created_at, 'd/m/Y') }} <a class="link post-title" href="{{ route('post', $post->slug) }}">{{ $post->title }}</a></h2>
+            <h5 class="temas">en
+                <a class="post-tema link" href="{{ route('category', $post->post_category->slug) }}">{{ $post->post_category->name }}</a>
                 <i class="ti-timer mr-1"> </i> {{readDuration($post->title, $post->content)}} @choice('min|mins', readDuration($post->title, $post->content))
             </h5>
-            <div class="fakeimg">
-                <img src="{{ asset('images/posts/'.$post->featured_image) }}" alt="{{$post->featured_image}}">
+            <div class="image">
+                @php
+                    $imagePath = public_path('images/posts/resized/resized_' . $post->featured_image);
+                    $imageUrl = asset('images/posts/resized/resized_' . $post->featured_image);
+                @endphp
+
+                @if (file_exists($imagePath))
+                    <img src="{{ $imageUrl }}" alt="{{ $post->title }}" id="myImg" onclick="openModal(this)" >
+                @else
+                    <img src="{{ asset('images/posts/' . $post->featured_image) }}" alt="{{ $post->title }}" id="myImg" onclick="openModal(this)">
+                @endif
             </div>
             {!!Str::ucfirst(words($post->content, 60))!!}
-            <a class="link" href="{{ route('post', $post->slug) }}">Llegir més</a>
+            <a class="link post-tema" href="{{ route('post', $post->slug) }}">Llegir més</a>
         </article>
+        <x-modal />
     @empty
         <div class="card">
             <h3>No hi ha articles relacionats amb la búsqueda.</h3>
