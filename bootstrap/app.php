30c95584fe83d0f069bdc32e4c46b67915678d6b
Juanmi <juanmi0802@gmail.com>
2025-08-25 17:56:21 +0200

    error 404 code

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,5 +20,9 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
+
+        if ($exceptions instanceof ModelNotFoundException || $exceptions instanceof NotFoundHttpException) {
+            return response()->view('errors.404', [], 404);
+        }
         //
     })->create();
